e1c396f3ed4b0e1d1095164f2f878f84bf84164f
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-03-26 06:01:19 +0530

    added: prisma service layer

---
+++ prisma/migrations/20250326001633_init_doctor_schema/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "doctors" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "doctors_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctors_username_key" ON "doctors"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctors_email_key" ON "doctors"("email");
